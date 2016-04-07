f8993d51a7e5821ee16e1e8a7ba925b42344d6d6
Simon Cropp <simon.cropp@gmail.com>
2016-04-07 21:05:49 +1000

    update to core v6

--- src/WireCompatTest/Version_6_0/Program.cs
+++ src/WireCompatTest/Version_6_0/Program.cs
@@ -13,15 +13,14 @@ class Program
 
     static async Task AsyncMain()
     {
-        var bus = await CreateBus(); 
+        var bus = await CreateBus();
         TestRunner.EndpointName = endpointName;
         await TestRunner.RunTests(bus);
     }
 
     static Task<IEndpointInstance> CreateBus()
     {
-        var busConfiguration = new EndpointConfiguration();
-        busConfiguration.EndpointName(endpointName);
+        var busConfiguration = new EndpointConfiguration(endpointName);
         busConfiguration.Conventions().ApplyMessageConventions();
         busConfiguration.UseSerialization<JsonSerializer>();
         busConfiguration.UseTransport<MsmqTransport>();
@@ -31,7 +30,7 @@ class Program
         busConfiguration.ScaleOut().InstanceDiscriminator("1");
         busConfiguration.RegisterComponents(c => c.ConfigureComponent<EncryptionVerifier>(DependencyLifecycle.SingleInstance));
         busConfiguration.EnableInstallers();
-        
+
         return Endpoint.Start(busConfiguration);
     }
 }
