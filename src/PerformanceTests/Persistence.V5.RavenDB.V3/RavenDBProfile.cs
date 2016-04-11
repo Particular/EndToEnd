7ab050b31df7db5e655e20ba2f4933fa39e59878
Ramon Smits <ramon.smits@gmail.com>
2016-04-11 14:41:53 +0200

    RavenDB connection string  configuration now read from `RavenDB` key.

--- src/PerformanceTests/Persistence.V5.RavenDB.V3/RavenDBProfile.cs
+++ src/PerformanceTests/Persistence.V5.RavenDB.V3/RavenDBProfile.cs
@@ -1,10 +1,23 @@
-﻿using NServiceBus;
+﻿using System;
+using System.Configuration;
+using NServiceBus;
 using NServiceBus.Persistence;
+using NServiceBus.RavenDB;
 
 class RavenDBProfile : IProfile
 {
-    public void Configure(BusConfiguration busConfiguration)
+    public void Configure(BusConfiguration cfg)
     {
-        busConfiguration.UsePersistence<RavenDBPersistence>();
+        var value = ConfigurationManager.ConnectionStrings["RavenDB"];
+
+        if (value == null) throw new InvalidOperationException("Connection string 'RavenDB' not configured.");
+
+        var connectionParameters = new ConnectionParameters
+        {
+            Url = value.ConnectionString
+        };
+
+        cfg.UsePersistence<RavenDBPersistence>()
+           .SetDefaultDocumentStore(connectionParameters);
     }
 }
