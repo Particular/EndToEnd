0b813e8f604c6872eab8814cdd08e5f99dfafcaa
Ramon Smits <ramon.smits@gmail.com>
2016-04-11 10:31:22 +0200

    Configuration for NHibernate persistence.

--- src/PerformanceTests/Persistence.V5.NHibernate.V6/NHibernateProfile.cs
+++ src/PerformanceTests/Persistence.V5.NHibernate.V6/NHibernateProfile.cs
@@ -1,9 +1,17 @@
-﻿using NServiceBus;
+﻿using System;
+using System.Configuration;
+using NServiceBus;
+using NServiceBus.Persistence;
 
 class NHibernateProfile : IProfile
 {
     public void Configure(BusConfiguration busConfiguration)
     {
-        busConfiguration.UsePersistence<NHibernatePersistence>();
+        var value = ConfigurationManager.ConnectionStrings["NHibernate"];
+        if (ConfigurationManager.ConnectionStrings == null) throw new InvalidOperationException("Connection string 'NHibernate' not configured.");
+
+        busConfiguration
+            .UsePersistence<NHibernatePersistence>()
+            .ConnectionString(value.ConnectionString);
     }
 }
