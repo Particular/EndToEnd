3fd4633d6eca8ec4fcb3a6792c77052930e61e46
WilliamBZA <william.brander@gmail.com>
2016-04-29 20:27:51 +0200

    Add FLR to Azure Storage Queues to bypass throttling 500 errors

--- src/PerformanceTests/Transport.V5.AzureStorageQueues.V6/AzureStorageQueueProfile.cs
+++ src/PerformanceTests/Transport.V5.AzureStorageQueues.V6/AzureStorageQueueProfile.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using NServiceBus;
 using Variables;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using System.Configuration;
 
 class AzureStorageQueueProfile : IProfile, INeedContext
 {
@@ -11,7 +14,30 @@ class AzureStorageQueueProfile : IProfile, INeedContext
         if ((int)MessageSize.Medium < (int)Context.Permutation.MessageSize) throw new NotSupportedException($"Message size {Context.Permutation.MessageSize} not supported by ASQ.");
 
         busConfiguration
+            .CustomConfigurationSource(new FlrConfig());
+
+        busConfiguration
             .UseTransport<AzureStorageQueueTransport>()
             .ConnectionString(ConfigurationHelper.GetConnectionString("AzureStorageQueue"));
+    }
+}
+
+class FlrConfig : IConfigurationSource
+{
+    public T GetConfiguration<T>() where T : class, new()
+    {
+        //To Provide FLR Config
+        if (typeof(T) == typeof(TransportConfig))
+        {
+            TransportConfig flrConfig = new TransportConfig
+            {
+                MaxRetries = 2
+            };
+
+            return flrConfig as T;
+        }
+
+        // To in app.config for other sections not defined in this method, otherwise return null.
+        return ConfigurationManager.GetSection(typeof(T).Name) as T;
     }
 }
