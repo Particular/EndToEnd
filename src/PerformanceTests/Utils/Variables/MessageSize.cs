283b1708bd17d5ab72de14e3690cdf25f38c4086
Ramon Smits <ramon.smits@gmail.com>
2016-05-11 19:14:26 +0200

    Additional messages size to make comparison easier with results from @danielmarbach

--- src/PerformanceTests/Utils/Variables/MessageSize.cs
+++ src/PerformanceTests/Utils/Variables/MessageSize.cs
@@ -4,7 +4,14 @@ namespace Variables
     {
         Tiny = 0,
         Small = 1024,
-        Medium = 10 * 1024,
-        Large = 100 * 1024
+        Medium = 10 * Small,
+        Large = 100 * Small,
+        L256 = 256 * Small,
+        L512 = 512 * Small,
+        L1MB = 1024 * Small,
+        L4MB = 4 * L1MB,
+        L16MB = 16 * L1MB,
+        L64MB = 64 * L1MB,
+         
     }
 }
