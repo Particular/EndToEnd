80c44abb2ce1e86e8a8f901181fd6ac0261a4bd2
Ramon Smits <ramon.smits@gmail.com>
2016-09-29 09:20:19 +0200

    Added ArtifactsParser project

---
+++ src/PerformanceTests/ArtifactsParser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArtifactsParser
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            var csv = ScanLogs.ToCsvString(path);
+            var dst = Path.Combine(path, "report.csv");
+            File.WriteAllText(dst, csv);
+            Console.WriteLine("Parsed '{0}' recursively and written to '{0}'", path, dst);
+        }
+    }
+}
