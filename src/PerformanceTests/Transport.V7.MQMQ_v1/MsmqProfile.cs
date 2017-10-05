d46fc4e30b1a76d1396a989f68bb5d1349b16e08
Ramon Smits <ramon.smits@gmail.com>
2017-10-06 01:49:18 +0200

    Added v7 framework host (clone of 6) + MSMQ transport
    Some new dependencies are needed like the metrics and performance counters package as these have been extracted from core. MSMQ is now also a separate package and used in the MSMQ transport library.

---
+++ src/PerformanceTests/Transport.V7.MQMQ_v1/MsmqProfile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using NServiceBus;
+using NServiceBus.Logging;
+using Tests.Permutations;
+using Variables;
+
+class MsmqProfile : IProfile, INeedPermutation
+{
+    ILog Log = LogManager.GetLogger(nameof(MsmqProfile));
+
+    public Permutation Permutation { private get; set; }
+
+    public void Configure(EndpointConfiguration endpointConfiguration)
+    {
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        transport.ConnectionString(ConfigurationHelper.GetConnectionString("MSMQ"));
+
+        if (Permutation.TransactionMode != TransactionMode.Default
+            && Permutation.TransactionMode != TransactionMode.None
+            && Permutation.TransactionMode != TransactionMode.Receive
+            && Permutation.TransactionMode != TransactionMode.Atomic
+            && Permutation.TransactionMode != TransactionMode.Transactional
+            ) throw new NotSupportedException("TransactionMode: " + Permutation.TransactionMode);
+
+        if (Permutation.TransactionMode != TransactionMode.Default) transport.Transactions(Permutation.GetTransactionMode());
+
+        RunInspections();
+    }
+
+    long SizeThreshold = 1024 * 1024 * 1024; // 1GB
+    long CountThreshold = 100000;
+
+    void RunInspections()
+    {
+        try
+        {
+            long size, count;
+
+            using (var bytes = new PerformanceCounter("MSMQ Service", "Total bytes in all queues"))
+            {
+                size = bytes.RawValue;
+            }
+
+            using (var messages = new PerformanceCounter("MSMQ Service", "Total messages in all queues"))
+            {
+                count = messages.RawValue;
+            }
+
+            Log.InfoFormat("MSMQ Currently contains {0:N0} messages, occupying {1:N0} bytes", count, size);
+
+            if (count > CountThreshold || size > SizeThreshold)
+            {
+                Log.WarnFormat("MSMQ message count ({0:N0}) or size ({1:N0}) exceeded. Please verify if MSMQ has a lot of (journaled) messages or message in the system (transactional) dead letter queue.", CountThreshold, SizeThreshold);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Debug("Optional MSMQ inspections failed to run.", ex);
+        }
+    }
+}
