874885334dd249f0f855ce39ee68c37d250e3bf1
Ramon Smits <ramon.smits@gmail.com>
2017-10-08 13:48:12 +0200

    Added Transport.V7.SQLServer.V4

---
+++ src/PerformanceTests/Transport.V7.SQLServer.V4/SqlServerProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+using Tests.Permutations;
+using Variables;
+
+class SqlServerProfile : IProfile, ISetup, INeedPermutation
+{
+    public Permutation Permutation { private get; set; }
+
+    public void Configure(EndpointConfiguration endpointConfiguration)
+    {
+        var transport = endpointConfiguration
+            .UseTransport<SqlServerTransport>();
+
+        transport
+            .DefaultSchema("V7")
+            .ConnectionString(ConfigurationHelper.GetConnectionString(Permutation.Transport.ToString()));
+
+        if (Permutation.TransactionMode != TransactionMode.Default
+            && Permutation.TransactionMode != TransactionMode.None
+            && Permutation.TransactionMode != TransactionMode.Receive
+            && Permutation.TransactionMode != TransactionMode.Atomic
+            && Permutation.TransactionMode != TransactionMode.Transactional
+            ) throw new NotSupportedException("TransactionMode: " + Permutation.TransactionMode);
+
+        if (Permutation.TransactionMode != TransactionMode.Default) transport.Transactions(Permutation.GetTransactionMode());
+    }
+
+    void ISetup.Setup()
+    {
+        var cs = ConfigurationHelper.GetConnectionString(Permutation.Transport.ToString());
+        var sql = Assembly.GetExecutingAssembly().GetManifestResourceText("Transport.V7.SQLServer.init.sql");
+        SqlHelper.CreateDatabase(cs);
+        SqlHelper.ExecuteScript(cs, sql);
+    }
+}
