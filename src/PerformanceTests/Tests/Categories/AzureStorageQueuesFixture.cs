20fdc811683366c045ff6d58f24b2e93ebf1daf2
Ramon Smits <ramon.smits@gmail.com>
2017-10-09 16:15:47 +0200

    Added Transport.V7.AzureStorageQueues.V8 , copied profile from V7 didn't work. Had to add `SanitizeQueueNamesWith` filer.

---
+++ src/PerformanceTests/Tests/Categories/AzureStorageQueuesFixture.cs
@@ -0,0 +1,55 @@
+namespace Categories
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Tests.Permutations;
+    using Variables;
+
+    [TestFixture(Description = "Transports", Category = "Performance"), Explicit]
+    public class AzureStorageQueuesFixture : Base
+    {
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void GatedSendLocalRunner(Permutation permutation)
+        {
+            base.GatedSendLocalRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void GatedPublishRunner(Permutation permutation)
+        {
+            base.GatedPublishRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void ReceiveRunner(Permutation permutation)
+        {
+            base.ReceiveRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void PublishOneOnOneRunner(Permutation permutation)
+        {
+            base.PublishOneOnOneRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void SendLocalOneOnOneRunner(Permutation permutation)
+        {
+            base.SendLocalOneOnOneRunner(permutation);
+        }
+
+        static IEnumerable<Permutation> CreatePermutations()
+        {
+            return PermutationGenerator.Generate(new Permutations
+            {
+                Versions = new[] { NServiceBusVersion.V7 },
+                Transports = new[] { Transport.AzureStorageQueues },
+                MessageSizes = new[] { MessageSize.Tiny },
+                Serializers = new[] { Serialization.Json },
+                OutboxModes = new[] { Outbox.Off },
+                ConcurrencyLevels = new[] { ConcurrencyLevel.EnvCores04x },
+                TransactionMode = new[] { TransactionMode.Receive, TransactionMode.None }
+            });
+        }
+    }
+}
---
+++ src/PerformanceTests/Transport.V7.AzureStorageQueues.V8/AzureStorageQueuesProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NServiceBus;
+using Variables;
+
+class AzureStorageQueuesProfile : IProfile, INeedContext
+{
+    public IContext Context { private get; set; }
+
+    public void Configure(EndpointConfiguration endpointConfiguration)
+    {
+        if ((int)MessageSize.Medium < (int)Context.Permutation.MessageSize) throw new NotSupportedException($"Message size {Context.Permutation.MessageSize} not supported by ASQ.");
+
+        var transport = endpointConfiguration
+            .UseTransport<AzureStorageQueueTransport>()
+            .SanitizeQueueNamesWith(queueName => queueName.Replace('.', '-'));
+
+        transport
+            .ConnectionString(ConfigurationHelper.GetConnectionString("AzureStorageQueue"));
+
+        var Permutation = Context.Permutation;
+
+        if (Permutation.TransactionMode != TransactionMode.Default
+            && Permutation.TransactionMode != TransactionMode.None
+            && Permutation.TransactionMode != TransactionMode.Receive
+            ) throw new NotSupportedException("TransactionMode: " + Permutation.TransactionMode);
+
+        if (Permutation.TransactionMode != TransactionMode.Default) transport.Transactions(Permutation.GetTransactionMode());
+
+    }
+
+}
