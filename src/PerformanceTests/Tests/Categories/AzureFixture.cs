162faf1ef922611d1052695d13eb0ea591123f6c
Ramon Smits <ramon.smits@gmail.com>
2017-10-15 14:00:23 +0200

    Extended azure fixture to smoke test the saga scenarios for it across all versions.

--- src/PerformanceTests/Tests/Categories/AzureFixture.cs
+++ src/PerformanceTests/Tests/Categories/AzureFixture.cs
@@ -9,22 +9,34 @@ namespace Categories
     public class AzureFixture : Base
     {
         [TestCaseSource(nameof(CreatePermutations))]
+        public void SagaUpdateRunner(Permutation permutation)
+        {
+            Tasks(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
         public override void SagaInitiateRunner(Permutation permutation)
         {
             base.SagaInitiateRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public void SagaCongestionRunner(Permutation permutation)
+        {
+            Tasks(permutation);
         }
 
         static IEnumerable<Permutation> CreatePermutations()
         {
             return PermutationGenerator.Generate(new Permutations
             {
-                Versions = new[] { NServiceBusVersion.V7 },
+                Versions = new[] { NServiceBusVersion.V7, NServiceBusVersion.V6, NServiceBusVersion.V5 },
                 Transports = new[] { Transport.MSMQ },
                 Persisters = new[] { Persistence.Azure },
                 Serializers = new[] { Serialization.Json },
                 OutboxModes = new[] { Outbox.Off },
                 TransactionMode = new [] { TransactionMode.Receive },
-                ConcurrencyLevels = new[] { ConcurrencyLevel.EnvCores04x }
+                ConcurrencyLevels = new[] { ConcurrencyLevel.Sequential }
             });
         }
     }
