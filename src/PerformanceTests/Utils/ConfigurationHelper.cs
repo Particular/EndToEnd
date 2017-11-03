c98761164be2a19f93859be17958759aa8405c02
Ramon Smits <ramon.smits@gmail.com>
2017-11-03 11:31:02 +0100

    Only logging full connection string for debug logging to not expose credential under normal runs.

--- src/PerformanceTests/Utils/ConfigurationHelper.cs
+++ src/PerformanceTests/Utils/ConfigurationHelper.cs
@@ -4,22 +4,25 @@ using NLog;
 
 public static class ConfigurationHelper
 {
-    static readonly ILogger Log = LogManager.GetLogger(nameof(Configuration));
+    static readonly ILogger Log = LogManager.GetLogger(nameof(ConfigurationHelper));
 
     public static string GetConnectionString(string connectionStringName)
     {
         var environmentVariableConnectionString = Environment.GetEnvironmentVariable(connectionStringName);
         if (!string.IsNullOrWhiteSpace(environmentVariableConnectionString))
         {
-            Log.Info("Environment variable found {0}", environmentVariableConnectionString);
+            Log.Info("Environment variable found.");
+            Log.Debug("Connection string: {0}", environmentVariableConnectionString);
             return environmentVariableConnectionString;
         }
 
         var applicationConfigConnectionString = ConfigurationManager.ConnectionStrings[connectionStringName];
         if (applicationConfigConnectionString != null)
         {
-            Log.Info("App.config connection string variable found {0}", environmentVariableConnectionString);
-            return applicationConfigConnectionString.ConnectionString;
+            var cs = applicationConfigConnectionString.ConnectionString;
+            Log.Info("App.config connection string variable found.");
+            Log.Debug("Connection string: {0}", cs);
+            return cs;
         }
 
         throw new NotSupportedException($"Connection string {connectionStringName} could not be resolved.");
@@ -28,23 +31,24 @@ public static class ConfigurationHelper
     public static string FetchSetting(string key)
     {
         string value;
-
-        value = Environment.GetEnvironmentVariable(key);
-
-        if (!string.IsNullOrWhiteSpace(value))
-        {
-            Log.Info("Setting: {0} = {1} ({2})", key, value, "Environment");
-            return value;
-        }
-
-        value = ConfigurationManager.AppSettings[key];
-
-        if (!string.IsNullOrWhiteSpace(value))
-        {
-            Log.Info("Setting: {0} = {1} ({2})", key, value, "AppSetting");
-            return value;
-        }
-
+        if (TryGetEnvVar(key, out value)) return value;
+        if (TryGetAppSetting(key, out value)) return value;
         return null;
+    }
+
+    static bool TryGetAppSetting(string key, out string value)
+    {
+        value = ConfigurationManager.AppSettings[key];
+        if (string.IsNullOrWhiteSpace(value)) return false;
+        Log.Debug("Setting: {0} = {1} ({2})", key, value, "AppSetting");
+        return true;
+    }
+
+    static bool TryGetEnvVar(string key, out string value)
+    {
+        value = Environment.GetEnvironmentVariable(key);
+        if (string.IsNullOrWhiteSpace(value)) return false;
+        Log.Debug("Setting: {0} = {1} ({2})", key, value, "Environment");
+        return true;
     }
 }
