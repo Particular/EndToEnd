d76be70d021185590d161336266391ae9a9975e5
Tim <bussmann.t@gmail.com>
2017-11-28 10:48:54 +0100

    add necessary platform checks

--- src/PerformanceTests/Utils/Powerplan.cs
+++ src/PerformanceTests/Utils/Powerplan.cs
@@ -1,9 +1,40 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Threading;
+using log4net;
 
 public class Powerplan
 {
-    public static Guid GetActive()
+    static ILog Log = LogManager.GetLogger(typeof(Powerplan));
+
+    public static void CheckPowerPlan()
+    {
+        if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+        {
+            //only supported on windows
+            return;
+        }
+
+        try
+        {
+            var highperformance = new Guid("8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c");
+            var id = Powerplan.GetActive();
+
+            Log.InfoFormat("Powerplan: {0}", id);
+
+            if (id != highperformance)
+            {
+                Log.WarnFormat("Power option not set to High Performance, consider setting it to high performance!");
+                Thread.Sleep(3000);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Debug("Powerplan check failed, ignoring", ex);
+        }
+    }
+
+    static Guid GetActive()
     {
         var ActiveScheme = Guid.Empty;
         var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(IntPtr)));
@@ -13,6 +44,8 @@ public class Powerplan
         return ActiveScheme;
     }
 
+#pragma warning disable PC003
     [DllImport("powrprof.dll")]
     static extern UInt32 PowerGetActiveScheme(IntPtr UserRootPowerKey, ref IntPtr ActivePolicyGuid);
+#pragma warning restore PC003
 }
--- src/PerformanceTests/Utils/SymbolicLink.cs
+++ src/PerformanceTests/Utils/SymbolicLink.cs
@@ -1,13 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 public class SymbolicLink
 {
     class Win32
     {
+#pragma warning disable PC003
         [DllImport("kernel32.dll")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, LinkType dwFlags);
+#pragma warning restore PC003
     }
 
     enum LinkType
@@ -18,6 +21,11 @@ public class SymbolicLink
 
     public static bool Create(string source, string destination)
     {
+        if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+        {
+            return false;
+        }
+
         if (!File.Exists(source)) throw new FileNotFoundException("Source file not found.", source);
         return Win32.CreateSymbolicLink(destination, source, LinkType.File);
     }
