16886c28c6d3d3bf4a5d7dad512314a50ae144fc
Tim <bussmann.t@gmail.com>
2017-12-06 13:18:32 +0100

    only test .net core compatible versions

--- src/PerformanceTests/Utils/Permutations/Permutations.cs
+++ src/PerformanceTests/Utils/Permutations/Permutations.cs
@@ -7,10 +7,10 @@ namespace Tests.Permutations
     {
         public Audit[] AuditModes = { Audit.Off };
         public MessageSize[] MessageSizes = { MessageSize.Tiny };
-        public NServiceBusVersion[] Versions = (NServiceBusVersion[])Enum.GetValues(typeof(NServiceBusVersion));
+        public NServiceBusVersion[] Versions = { NServiceBusVersion.V7 };
         public Outbox[] OutboxModes;
         public Persistence[] Persisters = { Persistence.InMemory };
-        public Platform[] Platforms = { Platform.NetFramework, Platform.NetCore };
+        public Platform[] Platforms = { Platform.NetCore };
         public Serialization[] Serializers = { Serialization.Json, };
         public Transport[] Transports = (Transport[])Enum.GetValues(typeof(Transport));
         public GarbageCollector[] GarbageCollectors = { GarbageCollector.Server };
